3b7be21d09f4a35df20222855ae8527dda191500
Sannasi <sannasichottu@gmail.com>
2022-12-23 01:03:07 +0530

    crud-mysql-client

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Home from './pages/Home.js'
+import AddEdit from './pages/AddEdit';
+import View from './pages/View';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <ToastContainer position='top-center' />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/addContact' component={AddEdit} />
+          <Route path='/update/:id' component={AddEdit} />
+          <Route path='/view/:id' component={View} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+
   );
 }
 
